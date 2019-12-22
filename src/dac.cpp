f1a62db688b56a3b0959ca0bf78c0bb17c603d13
Filippo Volpe <filippovolpe98@gmail.com>
2019-12-22 17:06:17 +0100

    swapperd the codes

--- src/dac.cpp
+++ src/dac.cpp
@@ -4,11 +4,11 @@ void dac_setout(const uint8_t which, const uint8_t value){
     spi.wait_available();
     switch(which){
         case DAC_THR:
-            spi.buf[0]=0x70|(value>>4);
+            spi.buf[0]=0xF0|(value>>4);
             spi.buf[1]=(value<<4);
             break;
         case DAC_OVD:
-            spi.buf[0]=0xF0|(value>>4);
+            spi.buf[0]=0x70|(value>>4);
             spi.buf[1]=(value<<4);
             break;
     }
